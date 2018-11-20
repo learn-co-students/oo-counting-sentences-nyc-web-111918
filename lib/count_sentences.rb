74616a36a413bf34545af16e1ca6925870021e55
Mahmoud Agamy <agamyv@gmail.com>
2018-11-20 23:20:21 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(",")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = 0 
+    self.each do |word|
+      count += 1 if ",?!.".include?(word[-1])
+    end
+    count
   end
 end
