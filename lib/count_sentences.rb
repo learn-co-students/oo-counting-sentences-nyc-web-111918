002dba5bc5a9e80a7632c0f3b93ca9aff4963e99
dhamilt9 <danhamilton16@gmail.com>
2018-11-20 16:27:43 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,19 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    self[self.length-1]=='.'
   end
 
   def question?
-
+    self[self.length-1]=='?'
   end
 
   def exclamation?
-
+    self[self.length-1]=='!'
   end
 
   def count_sentences
-
+    self.split(". ").join("! ").split("! ").join("? ").split("? ").length
   end
 end
