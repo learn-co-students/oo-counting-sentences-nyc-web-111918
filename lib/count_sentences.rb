341d51d2eeb9bffa052d4c359531aa64cbe3b8f6
Damon Collins <damon.c.p.collins@gmail.com>
2018-11-20 16:52:24 -0500

    created string class

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,28 @@
-require 'pry'
+
 
 class String
 
   def sentence?
-    
+    return true if self[-1] == "."
+    false
   end
 
   def question?
-
+    return true if self[-1] == "?"
+    false
   end
 
   def exclamation?
-
+    return true if self[-1] == "!"
+    false
   end
 
   def count_sentences
-
+    sym = %w(. ! ?)
+    count = 0
+    self.chars.each_with_index do |ch, i|
+      count += 1 if sym.include?(ch) && ch != self[i + 1]
+    end
+    count
   end
 end
