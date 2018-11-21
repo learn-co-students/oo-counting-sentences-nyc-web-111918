a19c1543a125fe293dfff7671cf5f2363502df97
Isaac Oseas <oseasisaac91@gmail.com>
2018-11-21 03:15:21 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,41 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?('.')
+    
     
   end
 
   def question?
+    self.end_with?('?')
 
   end
 
   def exclamation?
+    self.end_with?('!')
 
   end
+  
+  
+  #makes a sentence into an array based on punctuation
+  def sentence_to_array (string)
+  string_split = string.split('.')
+  string_split.map! do |words|
+     words.split('!')   
+  end
+  string_split.flatten!
+
+  string_split.map! do |words|
+     words.split('?')   
+  end
+ string_split.flatten
+end
 
   def count_sentences
-
+    sentence_to_array(self).count
+    
+    
+    
+  
   end
 end
