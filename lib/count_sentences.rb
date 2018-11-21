ac69675a51de710658f585823766d9bec6c87e6a
Matt Doering <ablueplatypus21@gmail.com>
2018-11-21 16:58:42 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,9 +17,13 @@ class String
   def count_sentences
     #think about how to split the sentence.
     # sent_split = self.split(/\.|\?|\!/)
-   self.split(".").join("! ").split("! ").join("? ").split("? ").length
     # sent_split.delete("")
     # sent_split.count
+
+    #this splits on a period, then joins on exclamation and a space ect..
+
+   self.split(".").join("! ").split("! ").join("? ").split("? ").length
+
   end
     #self.end_with?("!")
 end #end of class
